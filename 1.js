bd44197567148f25b650b3122211ab6a35b1f957
Dmitrii Selianin <dselianin@tripactions.com>
2023-03-03 10:09:26 +0100

    init

---
+++ 1.js
@@ -0,0 +1,7 @@
+import { x } from './2';
+import { Swiper } from 'swiper/react';
+
+export const func = () => {
+    console.log(Swiper);
+    return x + 10;
+};
---
+++ 1.test.js
@@ -0,0 +1,9 @@
+import { func } from './1';
+
+describe('1', () => {
+  describe('2', () => {
+    it('3', () => {
+        func();
+    });
+  });
+});
